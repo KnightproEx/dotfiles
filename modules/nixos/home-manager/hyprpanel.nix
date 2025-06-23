d5d757c0c1717b2c2e717f4a99a6c36c6bdcb8bc
Boon Hui <khongbh011201@gmail.com>
2025-06-23 23:55:38 +0800

    feat: update config

--- modules/nixos/home-manager/hyprpanel.nix
+++ modules/nixos/home-manager/hyprpanel.nix
@@ -1,6 +1,5 @@
 {inputs, ...}: {
   imports = [inputs.hyprpanel.homeManagerModules.hyprpanel];
-
   programs.hyprpanel = {
     enable = true;
     systemd.enable = true;
@@ -12,32 +11,53 @@
         "bar.layouts" = {
           "0" = {
             left = ["dashboard" "workspaces" "windowtitle"];
-            middle = ["media"];
-            right = ["volume" "systray" "clock" "battery" "notifications"];
+            middle = ["clock"];
+            right = ["systray" "network" "bluetooth" "volume" "battery" "notifications"];
           };
         };
       };
 
-      bar.launcher.autoDetectIcon = true;
-      bar.workspaces.show_icons = true;
-      bar.clock.format = "%a %b %d  %I:%M %p";
+      bar = {
+        launcher.autoDetectIcon = true;
+        clock.format = "%a %b %d  %I:%M %p";
+        workspaces = {
+          show_icons = true;
+        };
+      };
 
       menus.clock = {
         time = {
-          military = true;
-          hideSeconds = true;
+          military = false;
+          hideSeconds = false;
         };
-        weather.unit = "metric";
+        weather.enabled = false;
       };
 
       menus.dashboard.directories.enabled = false;
       menus.dashboard.stats.enable_gpu = true;
+      menus.dashboard = {
+        shortcuts.left.shortcut1 = {
+          icon = "";
+          tooltip = "Zen Browser";
+          command = "zen-beta";
+        };
+        shortcuts.left.shortcut2 = {
+          icon = "";
+          tooltip = "File Explorer";
+          command = "dolphin";
+        };
+      };
 
       theme = {
         name = "catppuccin_mocha";
         bar = {
           opacity = 80;
           outer_spacing = "0.2em";
+          buttons = {
+            padding_x = "0.4rem";
+            padding_y = "0.0rem";
+            background_opacity = 0;
+          };
         };
         font = {
           size = "0.9rem";
