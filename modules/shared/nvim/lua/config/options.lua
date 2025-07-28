5f73ee4dd422e9753de8814ddbd910c929a13cdb
Boon Hui <boonhui.khong@bigpayme.com>
2025-07-28 17:18:24 +0800

    feat(nvim): update neovim options

--- modules/shared/nvim/lua/config/options.lua
+++ modules/shared/nvim/lua/config/options.lua
@@ -1,28 +1,25 @@
-local opt = vim.opt
-local o = vim.o
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.cmd("let g:netrw_liststyle = 3")
 
-o.timeout = true
-o.timeoutlen = 500
--- o.laststatus = 3
-o.cursorline = true
-
-opt.relativenumber = false
-opt.number = true
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.autoindent = true
-opt.wrap = false
-opt.ignorecase = true
-opt.smartcase = true
-opt.termguicolors = true
-opt.background = "dark"
-opt.backspace = "indent,eol,start"
-opt.clipboard:append("unnamedplus")
-opt.splitright = true
-opt.splitbelow = true
-opt.scrolloff = 999
+-- vim.opt.laststatus = 3
+vim.opt.timeout = true
+vim.opt.timeoutlen = 500
+vim.opt.cursorline = true
+vim.opt.relativenumber = false
+vim.opt.number = true
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.scrolloff = 999
